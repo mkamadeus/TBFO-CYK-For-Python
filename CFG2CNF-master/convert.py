f5551809d1ee9aa7c5b42d49b4b02007eb18bb05
michaelhans <michaelhans777@gmail.com>
2019-11-25 22:12:47 +0700

    Update convert.py

--- CFG2CNF-master/convert.py
+++ CFG2CNF-master/convert.py
@@ -1,4 +1,23 @@
+# S -> AB
+# A -> CD | CF
+# B -> c | EB
+# C -> a
+# D -> b
+# E -> c
+# F -> AD
+
 released = {
+        "AB" : ["S"],
+        "CD" : ["A"],
+        "CF" : ["A"],
+        "c" : ["B","E"],
+        "EB" : ["B"],
+        "a" : ["C"],
+        "b" : ["D"],
+        "AD" : ["F"]
+}
+
+dict = {
 		"bA" : ["S"],
 		"aB" : ["S"],
 		"bAA" : ['A'],
@@ -13,23 +32,25 @@ for key in released:
     for i in range(len(released[key])):
         print(released[key][i],"->", key)
 
-def IsUseless (c):
-    count = 1
-    result = true
-    for key in released:
-        for char in key:
-            if (count > 1):
+# def IsUseless (c):
+#     count = 0
+#     for key in released:
+#         for char in key:
+#             if (c == char):
+#                 count += 1
+#     if (count == 1):
+#         return False
+#     else:
+#         return True
 
-        count += 1
-
-print()
-check = True
-# Elimination Useless Production
-for key in released:
-    for char in key:
-        print(char)
-        check = IsUseless(char)
-        if (check):
-            print("Unique")
-        else:
-            print("None")
+# print()
+# check = True
+# # Elimination Useless Production
+# for key in released:
+#     for char in key:
+#         print(char)
+#         check = IsUseless(char)
+#         if (check):
+#             print("Unique")
+#         else:
+#             print("None")
