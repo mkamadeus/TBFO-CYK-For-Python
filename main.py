668117c3c86a72aa758787e3ddb5ad031ea33951
michaelhans <michaelhans777@gmail.com>
2019-11-27 00:42:07 +0700

    Cobaa

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import codeTokenizer as ctok
 import cyk
+import sys
+
+# Load Chomsky Normal Form
+if len(sys.argv) > 1:
+    modelPath = str(sys.argv[1])
+else:
+	modelPath = 'out.txt'
+	
+cyk.LoadCNF(modelPath)
 
 # Tokenize
 tokenizedCode = ctok.tokenizeInput("input.txt")
