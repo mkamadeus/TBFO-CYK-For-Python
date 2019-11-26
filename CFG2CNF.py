f28a46385027333655f314817ee51f174b1f4c3f
Lionnarta <lionnarta.savirandy2000@gmail.com>
2019-11-27 02:02:52 +0700

    upd

--- CFG2CNF.py
+++ CFG2CNF.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		modelPath = str(sys.argv[1])
 	else:
-		modelPath = 'part.txt'
+		modelPath = 'CFGPart5.txt'
 	
 	K, V, Productions = helper.loadModel( modelPath )
 
